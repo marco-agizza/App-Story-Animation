49d89d4c6000ca6db8f1e343d3b73008d8629988
marco-agizza <marco.agizza@outlook.com>
2022-11-16 14:53:54 +0100

    Initial Commit

---
+++ NC1-AppStore/NC1_AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_AppStoreApp.swift
+//  NC1-AppStore
+//
+//  Created by Marco Agizza on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1_AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
