fc84126b8e3cf92628cc52049043da3eff85a5b6
marco-agizza <marco.agizza@outlook.com>
2022-11-24 21:40:06 +0100

    Text app story description reformatted

--- NC1-AppStore/Views/AppStoryBodyView.swift
+++ NC1-AppStore/Views/AppStoryBodyView.swift
@@ -12,17 +12,30 @@ struct AppStoryBodyView: View {
     let appStory: AppStory
     
     var body: some View {
-        Text("""
-            **There's nothing quite like** \
+        VStack(alignment: .leading){
+            Text("""
+            **There's nothing quite like**
+            """
+            ) +
+            Text(
+             """
             spending some quality time with family, all cosied up and enjoying a great movie or series together.\
-            So get the popcorn ready and settle in front of the screen with these fantastic family friendly picks.\n\n\
-            **Costume Quest** \n\
+            So get the popcorn ready and settle in front of the screen with these fantastic family friendly picks.\n
+            """
+            )
+            .foregroundColor(.gray)
+            Text("""
+            **Costume Quest** \n
+            """
+            ) +
+            Text("""
             Imagination is the weapon of choice against monsters invading the town of Auburn Hollow. Join Wren, Reynold,\
             Everett and Luew as they turn into
             """)
+            .foregroundColor(.gray)
+        }
         .padding()
-        .font(.body)
-        .font(.system(size: 22))
+        .font(.system(size: 22, design: .default))
     }
 }
 
